ab75d190518962584ee89e29cd2797ba9b58bdee
Jędrzej <34661811+Jwasik@users.noreply.github.com>
2020-11-21 19:16:27 +0100

    Basic Classes

---
+++ client_app/client_lib/Communication_Package.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace client_lib
+{
+    class Communication_Package
+    {
+        private enum Types : ushort
+        {
+            PING,
+            QUIT_SERVER,
+            QUIT_LOBBY,
+            QUIT_GAME
+        }
+        Types type;
+        public byte[] ToByteArray()
+        {
+            List<Byte> data = new List<byte>();
+            byte[] vs = BitConverter.GetBytes((ushort)this.type);
+            data.AddRange(vs);
+            return new byte[1];
+        }
+    }
+}
---
+++ client_app/client_lib/Game_Bomberman.cs
@@ -0,0 +1,17 @@
+﻿using client_lib;
+using System.Net.Sockets;
+
+namespace client_lib
+{
+    class Game_Bomberman : Game
+    {
+        public Game_Bomberman(NetworkStream stream) : base(stream)
+        {
+
+        }
+        public override void Run()
+        {
+
+        }
+    }
+}
