9ac22fece2bfa0bfed7548df34f4645a28aac4d2
Jędrzej <34661811+Jwasik@users.noreply.github.com>
2020-11-23 21:29:42 +0100

    Update Communication_Package.cs

--- client_app/client_lib/Communication_Package.cs
+++ client_app/client_lib/Communication_Package.cs
@@ -8,46 +8,100 @@ using System.Windows.Documents;
 namespace client_lib
 {
     class Communication_Package
-    {
-        private enum Types : ushort
-        {
+    {/*
             PING,
+            CHOOSE,
+            LOGIN,
+            LOGIN_CONFIRM,
+            LOGIN_REFUSE,
+            SIGNUP,
+            SIGNUP_CONFIRM,
+            SIGNUP_REFUSE,
+            GLOBAL_MESSAGE,
             QUIT_SERVER,
             QUIT_LOBBY,
-            QUIT_GAME
+            QUIT_GAME*/
+
+        public String dataString;
+        public byte[] data;
+
+        #region constructors
+        public Communication_Package(byte[] data)
+        {
+           this.data = data;
+           this.dataString = BitConverter.ToString(this.data);
         }
-
-        Types type;
-        private byte[] data;
-
+        #endregion
+        #region noargs
         public void SetTypePING()
         {
-            this.type = Communication_Package.Types.PING;
+            this.dataString = "<type>PING</type>";
         }
 
         public void SetTypeQUIT_SERVER()
         {
-            this.type = Communication_Package.Types.QUIT_SERVER;
+            this.dataString = "<type>QUIT_SERVER</type>";
         }
 
         public void SetTypeQUIT_LOBBY()
         {
-            this.type = Communication_Package.Types.QUIT_LOBBY;
+            this.dataString = "<type>QUIT_LOBBY</type>";
         }
 
         public void SetTypeQUIT_GAME()
         {
-            this.type = Communication_Package.Types.QUIT_GAME;
+            this.dataString = "<type>QUIT_GAME</type>";
+        }
+        #endregion
+        #region multipleargs
+        public void SetTypeCHOOSE(int id)
+        {
+            this.dataString = "<type>CHOOSE</type><arg1>"+ id.ToString() +"</arg1>";
         }
 
+        public void SetTypeLOGIN(String username, String password)
+        {
+            this.dataString = "<type>LOGIN</type><arg1>" + username + "</arg1><arg2>" + password + "</arg2>";
+        }
 
+        public void SetTypeLOGIN_CONFIRM(String username)
+        {
+            this.dataString = "<type>LOGIN_CONFIRM</type><arg1>"+ username +"</arg1>";
+        }
+
+        public void SetTypeLOGIN_REFUSE(String username, String reason)
+        {
+            this.dataString = "<type>LOGIN_REFUSE</type><arg1>"+ username +"</arg1><arg2>"+ reason +"</arg2>";
+        }
+
+        public void SetTypeSIGNUP(String username, String password)
+        {
+            this.dataString = "<type>SIGNUP</type><arg1>"+ username + "</arg1><arg2>"+ password +"</arg2>";
+        }
+
+        public void SetTypeSIGNUP_CONFIRM(String username)
+        {
+            this.dataString = "<type>SIGNUP_CONFIRM</type><arg1>"+ username +"</arg1>";
+        }
+        public void SetTypeSIGNUP_REFUSE(String username, String reason)
+        {
+            this.dataString = "<type>SIGNUP_REFUSE</type><arg1>"+ username +"</arg1><arg2>"+ reason +"</arg2>";
+        }
+        public void SetTypeGLOBAL_MESSAGE(int senderId, String senderUsername, String message)
+        {
+            this.dataString = "<type>GLOBAL_MESSAGE</type><arg1>"+ senderId.ToString() + "</arg1><arg2>" + senderUsername + "</arg2><arg3>" + message + "</arg3>";
+        }
+        #endregion
         public byte[] ToByteArray()
         {
-            List<Byte> data = new List<byte>();
-            byte[] vs = BitConverter.GetBytes((ushort)this.type);
+            byte[] result = Encoding.ASCII.GetBytes(dataString);
+            this.data = result;
+            return result;
+        }
 
-            data.AddRange(vs);
-            byte[] result = data.ToArray();
+        public String toString()
+        {
+            String result = BitConverter.ToString(data);
             return result;
         }
     }
